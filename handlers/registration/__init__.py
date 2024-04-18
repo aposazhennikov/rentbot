98ab37a30e0f7b5c44e754e3caf1b40d1b086015
hojo <hojo@list.ru>
2024-04-18 15:00:08 +0300

    little fixes with sctructure handlers

--- handlers/registration/__init__.py
+++ handlers/registration/__init__.py
@@ -1,45 +1,6 @@
-from fsm.registration import Registration
-from aiogram.fsm.context import FSMContext
-from models.user import User
-from aiogram import Bot, Dispatcher, html
-from aiogram.filters import CommandStart
-from aiogram.types import Message, CallbackQuery
-from aiogram import Router
-import json
-from titles import title
+from aiogram import Dispatcher
+from handlers.registration import start
 
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message, state: FSMContext) -> None:
-    user_manager = User()
-    get_user = user_manager.get_by_id(message.chat.id)
-
-    if (get_user):
-        message_out = title.load_title('welcome_back')
-        await message.answer(f"{message_out}, {html.bold(get_user['first_name'])}!")
-    else:
-        message_out = title.load_title('reg_first_name')
-
-        await message.answer(message_out)
-        await state.set_state(Registration.first_name)
-
-
-@dp.message(Registration.first_name)
-async def reg_two(message: Message, state: FSMContext):
-    await state.update_data(first_name=message.text)
-    await state.set_state(Registration.last_name)
-
-    message_out = title.load_title('reg_last_name')
-    await message.answer(message_out)
-
-
-@dp.message(Registration.last_name)
-async def reg_three(message: Message, state: FSMContext):
-    await state.update_data(last_name=message.text)
-    data = await state.get_data()
-
-    message_out = title.load_title('reg_finish')
-    await message.answer(f"{message_out} " + html.bold(f"{data['first_name']} {data['last_name']}"))
-    await state.clear()
+start.run(dp)
--- handlers/registration/start.py
+++ handlers/registration/start.py
@@ -1,7 +1,44 @@
 # Команда 'старт' запускает процесс обработки хэндлером следующих сообщений, если ID пользователя не в базе данных ->
 # Спрашиваем имя и переключаем FSM в первый режим - записи имени(first_name). Иначе, пишем сообщение,
 # что пользователь уже зарегистрирован и выводим меню.
+from fsm.registration import Registration
+from aiogram.fsm.context import FSMContext
+from models.user import User
+from aiogram import html
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from handlers.registration import start
+from titles import title
 
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message) -> None:
-    pass
+
+def run(dp):
+    @dp.message(CommandStart())
+    async def command_start_handler(message: Message, state: FSMContext) -> None:
+        user_manager = User()
+        get_user = user_manager.get_by_id(message.chat.id)
+
+        if (get_user):
+            message_out = title.load_title('welcome_back')
+            await message.answer(f"{message_out}, {html.bold(get_user['first_name'])}!")
+        else:
+            message_out = title.load_title('reg_first_name')
+
+            await message.answer(message_out)
+            await state.set_state(Registration.first_name)
+
+    @dp.message(Registration.first_name)
+    async def reg_two(message: Message, state: FSMContext):
+        await state.update_data(first_name=message.text)
+        await state.set_state(Registration.last_name)
+
+        message_out = title.load_title('reg_last_name')
+        await message.answer(message_out)
+
+    @dp.message(Registration.last_name)
+    async def reg_three(message: Message, state: FSMContext):
+        await state.update_data(last_name=message.text)
+        data = await state.get_data()
+
+        message_out = title.load_title('reg_finish')
+        await message.answer(f"{message_out} " + html.bold(f"{data['first_name']} {data['last_name']}"))
+        await state.clear()
