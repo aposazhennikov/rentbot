b533443d274ad43467010f5ee2087278b0ff7b46
Aleksandr Posazhennikov <152597614+aposazhennikov@users.noreply.github.com>
2024-04-21 22:34:34 +0300

    Update markup_registration.py sasha's review

--- markups/markup_registration.py
+++ markups/markup_registration.py
@@ -1,38 +1,37 @@
 from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup)
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+# Titles contains function load_json/load_title/save_user_language/get_user_language this is needed for
+# work with translations and response to users
 from titles import title
 
 
 async def inline_ntrp_quest(chat_id):
-    # way 1
+    ''' NEED TO ADD FUNCTION DESCRIPTION '''
+    
     title_btn_yes = title.load_title(chat_id, 'reg_ntrp_btn_yes')
     title_btn_no = title.load_title(chat_id, 'reg_ntrp_btn_no')
-    keyboard_builder = InlineKeyboardBuilder()
-
-    btn_yes = InlineKeyboardButton(
-        text=title_btn_yes, callback_data='reg_ntrpyes')
-    btn_no = InlineKeyboardButton(
-        text=title_btn_no, callback_data='reg_ntrpno')
-
-    keyboard_builder.add(btn_yes)
-    keyboard_builder.add(btn_no)
-    keyboard_builder.adjust(2).as_markup()
-
+    # way 1
+        # keyboard_builder = InlineKeyboardBuilder()
+        # btn_yes = InlineKeyboardButton(text=title_btn_yes, callback_data='reg_ntrpyes')
+        # btn_no = InlineKeyboardButton(text=title_btn_no, callback_data='reg_ntrpno')
+        # keyboard_builder.add(btn_yes)
+        # keyboard_builder.add(btn_no)
+        # keyboard_builder.adjust(2).as_markup()
+        # return keyboard_builder.adjust(2).as_markup()
     # way 2
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=title_btn_yes,
-                              callback_data='reg_ntrpyes'),
-         InlineKeyboardButton(text=title_btn_no, callback_data='reg_ntrpno')],
-    ])
+                        InlineKeyboardButton(text=title_btn_yes, callback_data='reg_ntrpyes'),
+                        InlineKeyboardButton(text=title_btn_no, callback_data='reg_ntrpno')])
     return keyboard
-    # return keyboard_builder.adjust(2).as_markup()
+   
 
 
 async def inline_ntrp_accept(chat_id):
+    ''' NEED TO ADD DESCRIPTION '''
     # way 2
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=title.load_title(chat_id, 'reg_ntrp_btn_accept'),
-                              callback_data='reg_ntrpyes'),],
-    ])
+                        InlineKeyboardButton(text=title.load_title(chat_id, 'reg_ntrp_btn_accept'),
+                              callback_data='reg_ntrpyes')])
     return keyboard
     # return keyboard_builder.adjust(2).as_markup()
